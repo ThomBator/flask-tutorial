81b3cf0e9624f8a45025d8d2e6362c0d7ae693d0
ThomBator <thom.bator@gmail.com>
2022-09-12 17:39:51 -0400

    added more routes, templates and css file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,16 +11,25 @@ def create_app(test_config=None):
         # loads the instance config, if it exists, when not testing 
         app.config.from_pyfile('config.py', silent=True)
     else:
+        #allows us to easily change config file later when we move to production 
         app.config.from_mapping(test_config)
-    
+    # This line is helpful for creating multiple instance of the same application with different configs etc. 
     try:
         os.makedirs(app.instance_path)
     except OSError:
-        pass 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, world!'
+        print("Error: instance path was not created.") 
+    
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
     return app 
 
